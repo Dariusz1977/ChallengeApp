efb564e1155d29184d7a9670a1437ec11684ff54
Dariusz1977 <drydzewski@world.pl>
2023-11-24 11:46:14 +0100

    Praca domowa dzien 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool Ewa = true;
+bool Darek = false;
+var womenAge = 33;
+var menAge = 17;
+
+
+if (Darek)
+{
+    if (menAge < 18)
+    {
+        Console.WriteLine("Darek jest niepełnoletni.");
+    }
+
+    else
+    {
+        Console.WriteLine("Darek jest pełnoletni i może iść na dyskotekę.");
+    }
+
+}
+else
+{
+    if (Ewa)
+    {
+        if (womenAge > 32)
+        {
+            Console.WriteLine("Ewa ma więcej niż 33 lata i nie wypada jej iść na dyskotekę :).   ");
+        }
+        else
+        {
+            Console.WriteLine("To jest Ewa i jest jeszcze młoda.");
+        }
+
+
+
+
+    }
+
+
+    else
+    {
+        Console.WriteLine("Proszę wpisać inne imię");
+    }
+
+}
